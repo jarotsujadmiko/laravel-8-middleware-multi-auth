54966627e1173c90af998222124612ee20ad710c
jarotsujadmiko <jarot.sujadmiko@gmail.com>
2021-04-15 22:21:09 +0700

    membuat home di admin dan user dan mengatur middleware admin dan user di route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +20,17 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::middleware(['Auth'])->group(function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::middleware(['admin'])->group(function(){
+        Route::get('admin',[AdminController::class,'index']);
+    });
+    Route::middleware(['user'])->group(function(){
+        Route::get('user',[UserController::class,'index']);
+    });
+    Route::get('/logout', function(){
+        Auth::logout();
+        redirect('/');
+    });
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
